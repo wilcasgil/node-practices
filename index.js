ebe89fdef9c9f7dc00200840145814838b914f75
Wilson Castro Gil <47094758+wilcasgil@users.noreply.github.com>
2020-05-16 20:30:30 -0500

    Project restruct
    add services and views

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
-const express = require("express");
-const path = require("path");   //path (modulo nativo) Para solucionar problemas de compatibilidad entre los directorios siempre lo ejecute en la ruta donde esa el aplicativo
-const app = express();
 
-const productsRouter = require('./routes/products');  //Incluir la ruta del router
-const productsApiRouter = require('./routes/api/products');  //Incluir la ruta de la api
+/**
+ * imports
+ * path (modulo nativo) Para solucionar problemas de compatibilidad entre los directorios siempre lo ejecute en la ruta donde esa el aplicativo
+ * middleware body-parse: Para procesar informacion que vien en el cuerpo de una resquest que vienen en formato json. Para enviar el product en formato json. express es muy liviano (basico).
+ */
+const express = require("express")
+const path = require("path")
+const bodyParser = require('body-parser')
 
-//use() metodo para usar middlewares (que prefijo va a tener "/static" y la ruta de los staticos con expres.static() con el path para no tener problemas a la hora de escuchar la ruta)
-app.use("/static", express.static(path.join(__dirname, "public")));
+/**
+ * relative routes
+ * ruta de las vistas
+ * ruta de la api
+ */
+const productsRouter = require('./routes/views/products')
+const productsApiRouter = require('./routes/api/products')
 
-app.set("views", path.join(__dirname, "views"));   //la carpeta de la aplicacion __dirname
-app.set("view engine", "pug");    //y el view engine sera pug, el cual viene implementado de forma nativa
+/**
+ * app initialization
+ */
+const app = express()
 
-//Establecer los endspoint de los productos
-app.use("/products", productsRouter);   //ruta de los productos
-app.use("/api/products", productsApiRouter);   //ruta de la api
+/**
+ * middlewares
+ * bodyParser.json(), para cuando se envia en un request pos se le envia la informacion en el body como un json,
+ * express la pueda leer ya que express por defecto no sabe que es un json y el tiene que saber como interpretarlo
+ */
+app.use(bodyParser.json())
 
+/**
+ * static files
+ * use() metodo para usar middlewares (que prefijo va a tener "/static" y la ruta de los staticos con expres.static() con el path para no tener problemas a la hora de escuchar la ruta)
+ * __dirname, expone el directorio (la ruta) donde esta corriendo el proceso de node y es mucho mas seguro que dejar la ruta relativa sola "public"
+ * path.join, concatena ese directorio donde corre ese proceso con la ruta a la que le queremos entregar,
+ * y se encarga de evitar esos problemas de seguridad que pueda tener los diferentes sistemas operativos
+ */
+app.use("/static", express.static(path.join(__dirname, "public")))
+
+/**
+ * view engine setup
+ * is pug, , el cual viene implementado de forma nativa
+ */
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
+
+/**
+ * routes
+ * establecer los endspoint de los productos en views y api
+ */
+app.use("/views/products", productsRouter)
+app.use("/api/products", productsApiRouter)
+
+/**
+ * redirect
+ */
+app.get('/', (req, res) => {
+  res.redirect('/products')
+} )
+
+/**
+ * server
+ */
 const server = app.listen(8000, () => {
-  console.log(`Listening http://localhost:${server.address().port}`);
-});
+  console.log(`Listening http://localhost:${server.address().port}`)
+})
